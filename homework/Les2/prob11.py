f24ec25be18ffd981e506d44f1e5294582941c82
VM42 <luckybastard20@gmail.com>
2020-11-02 22:44:41 +0600

    final01

---
+++ homework/Les2/prob11.py
@@ -0,0 +1,8 @@
+# Создать список
+
+just_list = [ 42, what, number, Is, it? ]
+def some_type():
+    for  in range(len(just_list)):
+        print(type(just_list[el]))
+    return
+my_type(just_list)
